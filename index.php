774f5153249385ceb8a3fef63fedb5a2dfbe1e40
Felipe Kennedy <felipekennedy990@gmail.com>
2024-11-26 21:53:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comandos</title>
+</head>
+<body>
+    <p>Aprendendo comandos de php basicos</p>
+    1-Sintaxe
+    no php pra escrevemos uma sintaxe devemos primeiro declarar lá como php pra isso usamos o comando < + ?
+
+    veremos um exemplo
+
+    <?php
+    echo"hello world";
+    ?>
+
+    2-agora iremos para o proximo exemplo que seria para mostrar que não importa se sua sintaxe for maiuscula ou miuscula ou se misturar os dois, se o codigo tiver certo isso que importa"<br>"
+    <?php
+ $color = "vermelho";
+ echo "meu novo mouse é " . $color ."<br>";
+?>
+<?php
+$manteiga = "margarina";
+echo "está acabando a " . $manteiga . "<br>";
+?>
+
+</body>
+</html>
